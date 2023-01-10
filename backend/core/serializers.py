9c68d87b484049efb9a6c114d1afa80dd002ad73
Marcin Jarosiński <marcin.jarosinski@stxnext.pl>
2023-01-10 19:52:20 +0100

    add TransactionSerializer and change field in Budget serializer

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,10 +1,17 @@
-from core.models import Budget
+from core.models import Budget, Transaction
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = "__all__"
+        ref_name = "Transaction serializer"
+
+
 class BudgetSerializer(serializers.ModelSerializer):
-    transaction = serializers.StringRelatedField(many=True)
+    transaction = TransactionSerializer(many=True)
 
     class Meta:
         model = Budget
