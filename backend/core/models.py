932d0c081fff1df251007ec937c91297f89c4845
Marcin Jarosiński <marcin.jarosinski@stxnext.pl>
2023-01-10 19:48:18 +0100

    fix related names in models

--- backend/core/models.py
+++ backend/core/models.py
@@ -6,8 +6,8 @@ from django.db import models
 
 class Budget(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ForeignKey(User, related_name="budget", on_delete=models.CASCADE)
-    allowed_to = models.ManyToManyField(User, related_name="budgets", null=True, blank=True)
+    user = models.ForeignKey(User, related_name="budgets", on_delete=models.CASCADE)
+    allowed_to = models.ManyToManyField(User, related_name="budget", null=True, blank=True)
 
     def __str__(self):
         return f"Budget of {self.user.username}"
@@ -33,9 +33,9 @@ class Transaction(models.Model):
         max_length=7,
         choices=TransactionType.choices,
     )
-    budget = models.ForeignKey(Budget, related_name="budget", on_delete=models.PROTECT)
+    budget = models.ForeignKey(Budget, related_name="transaction", on_delete=models.PROTECT)
     category = models.ForeignKey(
-        TransactionCategory, related_name="category", on_delete=models.PROTECT
+        TransactionCategory, related_name="transaction", on_delete=models.PROTECT
     )
 
     def __str__(self):
