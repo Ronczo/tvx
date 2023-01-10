4a9e23d3713427bdbee1073f2a73a57c5a4f5b59
Marcin Jarosiński <marcin.jarosinski@stxnext.pl>
2023-01-10 19:49:18 +0100

    add Budget filter

---
+++ backend/core/filters.py
@@ -0,0 +1,14 @@
+import django_filters
+from core.models import Budget
+
+
+class BudgetFilter(django_filters.FilterSet):
+    """Filter budget by category if any transaction in Budget includes given category"""
+
+    category = django_filters.CharFilter(
+        lookup_expr="icontains", field_name="transaction__category__name"
+    )
+
+    class Meta:
+        model = Budget
+        fields = ["category"]
