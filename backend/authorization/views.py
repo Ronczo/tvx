6f19d9347821851522a07c3769eb306766ad39c1
Marcin Jarosiński <marcin.jarosinski@stxnext.pl>
2023-01-10 18:02:03 +0100

    add simple view to test (WIP)

--- backend/authorization/views.py
+++ backend/authorization/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from authorization.serializers import UserSerializer
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from rest_framework import mixins, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+
+class UserViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
