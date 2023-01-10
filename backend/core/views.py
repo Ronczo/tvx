51d9ef77167cb6fb850c77c85f0078e39b1a9b18
Marcin Jarosiński <marcin.jarosinski@stxnext.pl>
2023-01-10 19:49:10 +0100

    add budget viewset

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from core.filters import BudgetFilter
+from core.models import Budget
+from core.serializers import BudgetSerializer
+from django.contrib.auth.models import User
+from django_filters import rest_framework
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+
+class BudgetViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    serializer_class = BudgetSerializer
+    queryset = Budget.objects.all()
+    filter_backends = (rest_framework.DjangoFilterBackend,)
+    filterset_class = BudgetFilter
+    filterset_fields = ("category",)
