98d68b0f71c322a2e6b66355c5063975a01501a4
Marcin Jarosiński <marcin.jarosinski@stxnext.pl>
2023-01-10 21:29:29 +0100

    fix urls

--- backend/tv/urls.py
+++ backend/tv/urls.py
@@ -19,8 +19,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("auth/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("auth/", include("authorization.urls")),
     path("api/", include("core.urls")),
 ]
