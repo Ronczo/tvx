e4aaf21a1d97ce9444342272896e72baefe2183e
Marcin Jarosiński <marcin.jarosinski@stxnext.pl>
2023-01-10 21:30:11 +0100

    add authentication class

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,12 +1,10 @@
 from core.filters import BudgetFilter
 from core.models import Budget
 from core.serializers import BudgetSerializer
-from django.contrib.auth.models import User
 from django_filters import rest_framework
 from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
-from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 class BudgetViewSet(
