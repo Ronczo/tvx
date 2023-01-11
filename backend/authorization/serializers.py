d08e674d7cb028461d8621a0977ea17ef7ce1ca5
Marcin Jarosiński <marcin.jarosinski@stxnext.pl>
2023-01-11 11:19:55 +0100

    add UserCreateSerializer

--- backend/authorization/serializers.py
+++ backend/authorization/serializers.py
@@ -5,5 +5,12 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "email"]
+        fields = ["id", "username"]
         ref_name = "User serializer"
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username", "password"]
+        ref_name = "User create serializer"
