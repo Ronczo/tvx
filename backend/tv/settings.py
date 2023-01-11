d101f5518b1f0754857a24662520fd540af84b46
Marcin Jarosiński <marcin.jarosinski@stxnext.pl>
2023-01-11 11:19:42 +0100

    add localhost to cors

--- backend/tv/settings.py
+++ backend/tv/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "localhost:3000"]
 
 
 # Application definition
